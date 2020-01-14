e800d45c080a2b111ef872100c88185c0ca5c4c6
wwow1 <43813285+wwow1@users.noreply.github.com>
2020-01-15 00:09:21 +0800

    Revert "Update mergesort.go"
    This reverts commit 32436a2f50598a599d23d40d7ec310c938d87c4d.

--- tidb/mergesort/mergesort.go
+++ tidb/mergesort/mergesort.go
@@ -1,52 +1,6 @@
 package main
 
-import "sync"
-func Divide(src []int64,start,end,depth int,wt *sync.WaitGroup){
-	if start>=end {
-		if depth==1 { //在16m的数组下是无用的,但保证程序可用性还是加了
-			wt.Done()
-		}
-		return
-	}
-	var mid int=(start+end)/2
-	Divide(src,start,mid,depth+1,wt)
-	Divide(src,mid+1,end,depth+1,wt)
-	Merge(src,start,mid,end)
-	if depth==1 {
-		wt.Done()
-	}
-}
-func Merge(src []int64,start,mid,end int){
-	tmp:=make([]int64,end-start+1)
-	i,j,k:=start,mid+1,0
-	for i<=mid && j<=end {
-		if src[i]<src[j] {
-			tmp[k]=src[i]
-			k++;i++
-		}else{
-			tmp[k]=src[j]
-			k++;j++
-		}
-	}
-	for i<=mid {
-		tmp[k]=src[i]
-		k++;i++
-	}
-	for j<=end {
-		tmp[k]=src[j]
-		k++;j++
-	}
-	for t:=0;t<k;t++ {
-		src[start+t]=tmp[t]
-	}
-}
+// MergeSort performs the merge sort algorithm.
+// Please supplement this function to accomplish the home work.
 func MergeSort(src []int64) {
-	length:=len(src)
-	var mid int=length/2
-	var wt sync.WaitGroup
-	wt.Add(2)
-	go Divide(src,0,mid,1,&wt)
-	go Divide(src,mid+1,length-1,1,&wt)
-	wt.Wait()
-	Merge(src,0,mid,length-1)
 }
